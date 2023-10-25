776ede236b1ae5b60b44ac7e663c1df4e2cc7d72
RLis <lisss17@mail.ru>
2023-10-25 19:21:15 +0500

    Updates

---
+++ precache-manifest.73d129f11d8672ee5c8e06d3a4acdf3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93157dcdf2570cf3c6db96398bb6f45b",
+    "url": "/F_product/index.html"
+  },
+  {
+    "revision": "be24010dd7e8efe74724",
+    "url": "/F_product/static/css/main.af5e3e84.chunk.css"
+  },
+  {
+    "revision": "ced09128940ea90cd89d",
+    "url": "/F_product/static/js/2.ceb81cc5.chunk.js"
+  },
+  {
+    "revision": "be24010dd7e8efe74724",
+    "url": "/F_product/static/js/main.dd66270a.chunk.js"
+  },
+  {
+    "revision": "ed0f4bc4137eceaf3e28",
+    "url": "/F_product/static/js/runtime~main.7de7c094.js"
+  },
+  {
+    "revision": "48980a6737d45f89ee5ceee6a8a851e4",
+    "url": "/F_product/static/media/check.48980a67.svg"
+  },
+  {
+    "revision": "0f84700da005742730dd7726981cd575",
+    "url": "/F_product/static/media/farmer.0f84700d.svg"
+  },
+  {
+    "revision": "5c6c03b1f9d230ded5553b7dcddbbf70",
+    "url": "/F_product/static/media/logo.5c6c03b1.svg"
+  }
+]);
